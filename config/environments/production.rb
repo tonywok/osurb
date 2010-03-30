ec6855549e4069be36d658365e7120210297a61e
tonyschneider <tsnydermtg@gmail.com>
2010-03-30 19:30:48 -0400

    action mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
-config.action_mailer.default_url_options = { :host => 'example.com' }
+config.action_mailer.default_url_options = { :host => 'osurb.heroku.com' }
 # See everything in the log (default is :info)
 # config.log_level = :debug
 
